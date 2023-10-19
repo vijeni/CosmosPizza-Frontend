003ccf7f07b4c37255eaa25633f94e512ff981ef
vainicius <comercial.viniciusgabriel@gmail.com>
2023-10-18 21:40:16 -0300

    feat: iniciando a tela de pessoasList.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,17 @@ import { IndexComponent } from './core/index/index.component';
 import { PedidosListComponent } from './views/pedidos/pedidos-list/pedidos-list.component';
 import { PedidosDetailsComponent } from './views/pedidos/pedidos-details/pedidos-details.component';
 import { ProdutoListComponent } from './views/produto/produto-list/produto-list.component';
+import { PessoasListComponent } from './views/pessoas/pessoas-list/pessoas-list.component';
 
 const routes: Routes = [
   {path: "",  redirectTo: "web", pathMatch: "full"},
   {path: "web",  component: IndexComponent, children: [
     {path: "pedidos", component: PedidosListComponent},
     {path: "pedido", component: PedidosDetailsComponent, children:[
-      {path: ":id", component: PedidosDetailsComponent},
       {path: "novo", component: PedidosDetailsComponent},
       {path: "editar/:id", component: PedidosDetailsComponent},
-      {path: "cancelar/:id", component: PedidosDetailsComponent}
+      {path: "cancelar/:id", component: PedidosDetailsComponent},
+      {path: ":id", component: PedidosDetailsComponent}
     ]},
     {path: "produtos", component: ProdutoListComponent},
     {path: "produto", component: ProdutoListComponent, children: [
@@ -21,7 +22,8 @@ const routes: Routes = [
       {path: "novo", component: ProdutoListComponent},
       {path: "editar/:id", component: ProdutoListComponent},
       {path: "cancelar/:id", component: ProdutoListComponent}
-    ]}
+    ]},
+    {path: "pessoas", component: PessoasListComponent}
   ]}
 ];
 
