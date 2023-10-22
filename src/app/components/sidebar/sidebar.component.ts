82020294d27d0a2e3bffe07852013bbbc2d731f3
vainicius <comercial.viniciusgabriel@gmail.com>
2023-10-22 01:14:53 -0300

    feat: rota pessoas na side-bar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -4,11 +4,13 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  imports: [NgbCollapseModule, MatSidenavModule, MatFormFieldModule, MatSelectModule, MatButtonModule],
+  imports: [NgbCollapseModule, MatSidenavModule, MatFormFieldModule, MatSelectModule, MatButtonModule, RouterModule // Adicione o RouterModule aos imports do módulo
+],
   standalone: true,
 
   styleUrls: ['./sidebar.component.scss']
--- src/app/views/pessoas/pessoas-list/pessoas-list.component.ts
+++ src/app/views/pessoas/pessoas-list/pessoas-list.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-pessoas-list',
   templateUrl: './pessoas-list.component.html',
-  styleUrls: ['./pessoas-list.component.scss']
+  styleUrls: ['./pessoas-list.component.scss'],
+
 })
 export class PessoasListComponent {
 
