6088a16c0f1eb80f36d2898e45dd1ddc2424780c
vainicius <comercial.viniciusgabriel@gmail.com>
2023-10-22 19:05:15 -0300

    feat: PessoasList getAll - integração Ok

--- src/app/views/pessoas/pessoas-list/pessoas-list.component.ts
+++ src/app/views/pessoas/pessoas-list/pessoas-list.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { Pessoa } from 'src/app/models/pessoa/pessoa';
+import { PessoaService } from 'src/app/services/pessoa/pessoa.service';
 
 @Component({
   selector: 'app-pessoas-list',
@@ -9,5 +11,23 @@ import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 })
 export class PessoasListComponent {
+pessoas : Pessoa[] = [];
+index! : number;
+service = inject(PessoaService);
+constructor() {
+  this.getAll();
+}
+  getAll(){
+    this.service.getAll().subscribe({
+      next: (pessoas) => {
+        this.pessoas = pessoas;
+      },
+      error: (erro) => {
+        console.log(erro.error);
+      },
+    });
+  }
+
+
 
 }
