1688fc5c518b73da7aa72c432bd6eebf36453fe3
vainicius <comercial.viniciusgabriel@gmail.com>
2023-10-26 00:01:29 -0300

    fix: rota getById/Deletar

--- src/app/services/sabor/sabor.service.ts
+++ src/app/services/sabor/sabor.service.ts
@@ -24,6 +24,6 @@ export class SaborService {
       return this.http.put<Sabor>(`${this.API}/editar/${id}`, pedido);
     }
     delete(id: number): Observable<any> {
-      return this.http.delete<any>(`${this.API}/delete/${id}`);
+      return this.http.delete<any>(`${this.API}/deletar/${id}`);
     }
 }
