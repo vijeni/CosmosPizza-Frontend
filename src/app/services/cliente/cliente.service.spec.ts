0a1bdb6cb5f25f6663a99f01076d022303a0e488
vainicius <comercial.viniciusgabriel@gmail.com>
2023-11-30 02:45:31 -0300

    tests: testes de usuario

--- src/app/services/cliente/cliente.service.spec.ts
+++ src/app/services/cliente/cliente.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClient, HttpClientModule } from '@angular/common/http';import { PessoaService } from './cliente.service';
+import { Cliente } from 'src/app/models/cliente/cliente';
+import { Endereco } from 'src/app/models/endereco/endereco';
 
 
 
@@ -19,4 +21,23 @@ describe('clienteservice', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  
+//   it('should retrieve a client by ID', () => {
+//     const dummyClient: Cliente = {
+//       nome: '',
+//       cpf: '',
+//       telefone: '',
+//       endereco: new Endereco(),
+//       id: 1,
+//       cadastro: new Date(),
+//     edicao: new Date(),
+//     delecao: new Date(),
+//       isAtivo: false
+//     };
+//     const clientId = 1;
+
+//     service.findById(clientId).subscribe(client => {
+//       expect(client).toEqual(dummyClient);
+//     });
+// });
 });
--- src/app/services/usuario/usuario.service.spec.ts
+++ src/app/services/usuario/usuario.service.spec.ts
@@ -1,18 +1,52 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { UsuarioService } from './usuario.service';
+import { Usuario } from 'src/app/models/usuario/usuario';
+import { List } from 'ng-bootstrap-icons/icons';
 
 describe('UsuarioService', () => {
   let service: UsuarioService;
+  let httpTestingController: HttpTestingController;
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({      imports: [HttpClientModule,HttpClientTestingModule]
 });
     service = TestBed.inject(UsuarioService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+
+  });
+
+  afterEach(() => {
+    // Verifique se não há solicitações pendentes e limpe-as
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('teste findById', () => {
+    const userId = 1;
+    const mockUsuario: Usuario = new Usuario();
+
+    service.findById(userId).subscribe(user => {
+      expect(user).toEqual(mockUsuario);
+    });
+
+    const req = httpTestingController.expectOne(`${service.API}/id/${userId}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUsuario);
+  });
+
+  it('teste de getAllUsuarios', () => {
+    const mockUsuarios = new Array<Usuario>;
+
+    service.getAll().subscribe(users => {
+      expect(users).toEqual(mockUsuarios);
+    });
+
+    const req = httpTestingController.expectOne(`${service.API}/todos`);
+    expect(req.request.method).toBe('GET');
   });
 });
